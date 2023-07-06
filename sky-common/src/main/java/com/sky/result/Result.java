551f8cbcd8c32e04b77ca215d4f36cbde35adc7e
fo <1745179058@qq.com>
2023-07-06 19:43:25 +0800

    修改登录密码校验

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 /**
  * 后端统一返回结果
+ *
  * @param <T>
  */
 @Data
@@ -29,7 +30,7 @@ public class Result<T> implements Serializable {
     }
 
     public static <T> Result<T> error(String msg) {
-        Result result = new Result();
+        Result<T> result = new Result();
         result.msg = msg;
         result.code = 0;
         return result;
