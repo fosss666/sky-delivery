99dc6da1e82d6d1b874b240e52a5f3fc283a1406
fo <1745179058@qq.com>
2023-07-17 11:31:56 +0800

    redis环境配置

---
+++ sky-server/src/main/java/com/sky/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package com.sky.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author: fosss
+ * Date: 2023/7/17
+ * Time: 10:49
+ * Description: redis配置类
+ */
+@Configuration
+@Slf4j
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        //设置redis的连接工厂对象
+        redisTemplate.setConnectionFactory(factory);
+        //设置key的序列化器
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置value的序列化器
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+}
