8357c9d10e7e7a4e456f4b566b6d44acb059e8cb
fo <1745179058@qq.com>
2023-07-17 12:05:27 +0800

    用户端获取店铺营业状态

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,10 +1,9 @@
-package com.sky.controller.admin;
+package com.sky.controller.user;
 
 import com.sky.constant.RedisConstant;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -15,27 +14,16 @@ import javax.annotation.Resource;
  * @author: fosss
  * Date: 2023/7/17
  * Time: 11:55
- * Description:
+ * Description:用户端店铺接口
  */
-@RestController("adminShopController")
-@RequestMapping("/admin/shop")
+@RestController("userShopController")
+@RequestMapping("/user/shop")
 @Slf4j
 @Api(tags = "店铺相关接口")
 public class ShopController {
 
     @Resource
     private RedisTemplate redisTemplate;
-
-    /**
-     * 设置店铺营业状态
-     */
-    @ApiOperation("设置店铺营业状态")
-    @PutMapping("/{status}")
-    public Result setShopStatus(@PathVariable("status") Integer status) {
-        log.info("设置店铺的营业状态为：{}", status == 1 ? "营业中" : "打烊中");
-        redisTemplate.opsForValue().set(RedisConstant.SHOP_STATUS_KEY, status);
-        return Result.success();
-    }
 
     /**
      * 获取店铺营业状态
